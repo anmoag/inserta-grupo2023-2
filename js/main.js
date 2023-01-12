9b5b2f485d92cf032a863a58f6f8fbe79eea05e2
anmoag <anmoag5@gmail.com>
2023-01-12 20:34:12 +0100

    incluyo idea html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+
+//crea dos funciones 
 function sorteoAmigoInvisible(){
-    let participantesRestantes = [];
-    var reciboParticipante = document.querySelector("#participantes").value();
-    participantesRestantes.push(reciboParticipante);
-   var participanteAleatorio1 = Match.floor(Match.randon() * participantesRestantes.length);
+    let participantes = [];
+    var inputText = document.querySelector("#participantes").value;
+    participantes.push(inputText);
+    
+    
+    var participanteAleatorio1 = Match.floor(Match.randon() * participantesRestantes.length);
     var participanteAleatorio2 = Match.floor(Match.randon() * participantesRestantes.length);
     }
