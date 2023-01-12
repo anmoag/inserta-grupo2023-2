76832f077ae69129e557103806f3492695127593
joseantonio56 <amayapostig@gmail.com>
2023-01-12 14:17:47 +0100

    subido el js hasta ahora

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function sorteoAmigoInvisible(){
     let participantesRestantes = [];
-    var reciboParticipante = document.querySelector("#").value();
+    var reciboParticipante = document.querySelector("#participantes").value();
     participantesRestantes.push(reciboParticipante);
-   
-    
+   var participanteAleatorio1 = Match.floor(Match.randon() * participantesRestantes.length);
+    var participanteAleatorio2 = Match.floor(Match.randon() * participantesRestantes.length);
     }
