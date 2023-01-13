1646bc79905fd04079d7da8dd3e5e176ca512326
anmoag <anmoag5@gmail.com>
2023-01-13 09:24:55 +0100

    informacion para el js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-//crea dos funciones 
+//debeis crear dos funciones en la primera (crearArray) recibe un string con los nombres  separados por comas con .split separarlos "," y con .push meterlos en el array una vez creado el array retornarlo como parametro y en la segunda funcion (hacerSorteo) recibir el parametro y condicionar el array para que haga el sorteo.
 function sorteoAmigoInvisible(){
     let participantes = [];
     var inputText = document.querySelector("#participantes").value;
