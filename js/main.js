9ed8a872524e40f8c3e7257a840426d7a17cafdb
joseantonio56 <amayapostig@gmail.com>
2023-01-13 09:44:34 +0100

    creada la function crearArray()

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 
 //crea dos funciones 
-function sorteoAmigoInvisible(){
-    let participantes = [];
-    var inputText = document.querySelector("#participantes").value;
+function crearArray(){
+    let participantes = [];//creo el array
+    var inputText = document.querySelector("#participantes").value;//recojo el valor de participante
+
+    if (inputText == "") {//veo a ver si esta vacio o no
+        alert("debes de poner los participantes");
+     }else{
     participantes.push(inputText);
+    console.log(participantes);//compruebo el array 
+    document.querySelector("#participantes").value = "";//vacio el input
+
+    }
     
-    
+    /*
     var participanteAleatorio1 = Match.floor(Match.randon() * participantesRestantes.length);
     var participanteAleatorio2 = Match.floor(Match.randon() * participantesRestantes.length);
+    */
     }
