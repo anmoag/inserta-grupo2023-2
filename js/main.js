c3dd390c6b600b07f6cbb1b3721cc68653c9354a
anmoag <anmoag5@gmail.com>
2023-01-15 12:13:47 +0100

    css y html completo

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
-
-  
- function sorteo() {
-    var participantes=document.querySelector("#participantes");
-    if (participantes.value == "") {
-        alert("Debes de poner los participantes");
-        participantes.focus();
-    } else {
-        var array = participantes.value.split(", ");
-        var arrayTextos = [];
-        while(array.length > 0){
-            var posicionAleatoria = Math.floor(Math.random() * array.length);
-            var participante = array[posicionAleatoria];
-            var array = array.splice(posicionAleatoria,1);
-
-          
-           
-        }
-    
-        var divContenedor = document.querySelector("#contenedor");
-        var lista = document.createElement("p");
-        divContenedor.appendChild(lista);
-        lista.innerText = participante + "regala a " 
-          
-    }
+function crearArray() {
+    stringParticipantes = document.querySelector("#participantes");
+    arrayParticipantes = stringParticipantes.value.split(",");
+    console.log(arrayParticipantes);
+    desordenarArray(arrayParticipantes);
 }
-           
+
+function desordenarArray(arrayParticipantes) {
+    var arrayDesordenado = arrayParticipantes.sort(() => Math.random() - 0.5);
+    console.log(arrayDesordenado);
+}
